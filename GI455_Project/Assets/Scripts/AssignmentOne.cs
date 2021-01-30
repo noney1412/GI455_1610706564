e605690863f8706e06a28a8673cc60b5176e78a8
DESKTOP-8OVKMJQ\nonpa <nonpanpila@gmail.com>
2021-01-30 15:28:10 +0700

    Send Assignment Week 1 ( Refactor Code )

---
+++ GI455_Project/Assets/Scripts/AssignmentOne.cs
@@ -0,0 +1,41 @@
+#region metadata
+/**
+#PRODUCT# 
+by #COMPANY#
+author: https://github.com/noney1412
+**/
+#endregion
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GI455
+{
+    public class AssignmentOne : MonoBehaviour
+    {
+        public List<string> infoList; 
+
+        public Text info;
+        public InputField input;
+        public Button submit;
+        public Text output;
+
+        private void Start()
+        {
+            infoList = new List<string>() { "Unity", "Unreal", "Resident Evil", "Google", "MongoDB" };
+            info.text = string.Join("\n", infoList);
+            submit.onClick.AddListener(OnSubmit);
+        }
+
+        public void OnSubmit()
+        {
+            bool isFound = infoList.Contains(input.text);
+
+            var hightlight = isFound ? "green" : "red";
+            var result = isFound ? "is found" : "is not found";
+            output.text = $" [ <color={hightlight}>{input.text}</color> ] {result} ";
+        }
+    }
+}
